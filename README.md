92a9f51d48f1a813a653622ddb7ee71131e28be7
Joe Baranski <joe.baranski49@gmail.com>
2020-05-17 00:45:21 -0400

    working heads or tails with percentage

---
+++ README.md
@@ -0,0 +1,16 @@
+_Looking for a shareable component template? Go here --> [sveltejs/component-template](https://github.com/sveltejs/component-template)_
+
+---
+
+# svelte app
+
+This is a project template for [Svelte](https://svelte.dev) apps. It lives at https://github.com/sveltejs/template.
+
+## Simple heads or tails flip
+
+- I really like Svelte; I'm gonna keep using it!
+
+### TODOs
+
+- Draw a heads and tails side of a coin and show the appropriate side when flipped
+- Add an animation to show the coin flip on button press
---
+++ src/App.svelte
@@ -0,0 +1,61 @@
+<script>
+  export let name;
+  let randomNum = undefined;
+  let tally = 0;
+  let headsCount = 0;
+  let tailsCount = 0;
+  let headsPercentage = 0;
+  let tailsPercentage = 0;
+  function flipCoin() {
+    randomNum = Math.random();
+    tally++;
+    if (randomNum < 0.5) {
+      headsCount++;
+      headsPercentage = Math.round((headsCount / tally) * 100);
+      tailsPercentage = 100 - headsPercentage;
+    } else {
+      tailsCount++;
+      tailsPercentage = Math.round((tailsCount / tally) * 100);
+      headsPercentage = 100 - tailsPercentage;
+    }
+  }
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <h1>Hello {name}, you cutie pie you 😗</h1>
+  {#if randomNum < 0.5}
+    <h2>Heads!</h2>
+  {/if}
+  {#if randomNum >= 0.5}
+    <h2>Tails!</h2>
+  {/if}
+  <button on:click={flipCoin}>Flip a coin!</button>
+
+  <h2>Total flips: {tally}</h2>
+  <section>
+    <p>heads percentage: {headsPercentage}%</p>
+    <p>tails percentage: {tailsPercentage}%</p>
+  </section>
+</main>
