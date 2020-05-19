8307825d80ed4d7952876724e682be30ff63e3f8
Joe Baranski <joe.baranski49@gmail.com>
2020-05-19 01:10:25 -0400

    reset button dun broke

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,14 @@
 
   let tails = "./images/tails-coin.png";
   let heads = "./images/heads-coin.png";
+
   let randomNum = undefined;
   let tally = 0;
   let headsCount = 0;
   let tailsCount = 0;
   let headsPercentage = 0;
   let tailsPercentage = 0;
+
   function flipCoin() {
     randomNum = Math.random();
     tally++;
@@ -22,6 +24,14 @@
       headsPercentage = 100 - tailsPercentage;
     }
   }
+  function reset() {
+    let randomNum = undefined;
+    let tally = 0;
+    let headsCount = 0;
+    let tailsCount = 0;
+    let headsPercentage = 0;
+    let tailsPercentage = 0;
+  }
 </script>
 
 <style>
@@ -32,11 +42,24 @@
     margin: 0 auto;
   }
 
-  h1 {
+  h2 {
     color: #ff3e00;
     text-transform: uppercase;
-    font-size: 4em;
+    font-size: 3em;
     font-weight: 100;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  img {
+    height: 240px;
+    width: 240px;
+    margin-bottom: 1rem;
   }
 
   @media (min-width: 640px) {
@@ -47,18 +70,21 @@
 </style>
 
 <main>
-  <h1>Hello {name}, you cutie pie you 😗</h1>
-  {#if randomNum < 0.5}
-    <img src={heads} alt="the words heads on a grey coin" />
-  {/if}
-  {#if randomNum >= 0.5}
-    <img src={tails} alt="the words tails on a grey coin" />
-  {/if}
-  <button on:click={flipCoin}>Flip a coin!</button>
+  <h2>Hello {name}, you cutie pie you 😗</h2>
+  <div>
+    {#if randomNum < 0.5}
+      <img src={heads} alt="the words heads on a grey coin" />
+    {/if}
+    {#if randomNum >= 0.5}
+      <img src={tails} alt="the words tails on a grey coin" />
+    {/if}
+    <button on:click={flipCoin}>Flip a coin!</button>
+  </div>
 
   <h2>Total flips: {tally}</h2>
   <section>
     <p>heads percentage: {headsPercentage}%</p>
     <p>tails percentage: {tailsPercentage}%</p>
   </section>
+  <button on:click={reset}>Reset?</button>
 </main>
